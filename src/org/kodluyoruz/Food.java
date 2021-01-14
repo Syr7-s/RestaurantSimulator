71083dc9f319eb907d4d29e2509065dbea28c46a
Syr7-s <isasayar7@github.com>
2021-01-14 14:15:56 +0300

    Food class was created with Thread Safe Singleton

--- src/org/kodluyoruz/Food.java
+++ src/org/kodluyoruz/Food.java
@@ -1,4 +1,18 @@
 package org.kodluyoruz;
 
 public class Food {
+    private static Food instance;
+    private static Object lock = new Object();
+
+    private Food() {
+
+    }
+
+    public static Food getInstance() {
+        synchronized (lock) {
+            if (instance == null)
+                return new Food();
+            return instance;
+        }
+    }
 }
