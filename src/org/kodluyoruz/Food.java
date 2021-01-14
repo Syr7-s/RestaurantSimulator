008e6612e8fc0ee823cb495e98680d2089a82a43
Syr7-s <isasayar7@github.com>
2021-01-14 14:19:12 +0300

    Food types was added to Food Class

--- src/org/kodluyoruz/Food.java
+++ src/org/kodluyoruz/Food.java
@@ -1,11 +1,15 @@
 package org.kodluyoruz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Food {
     private static Food instance;
     private static Object lock = new Object();
+    private static List<String> foods = new ArrayList<>();
 
     private Food() {
-
+        getAllWoods();
     }
 
     public static Food getInstance() {
@@ -14,5 +18,27 @@ public class Food {
                 return new Food();
             return instance;
         }
+    }
+    public List<String> getFoods(){
+        return foods;
+    }
+
+    private  void setFoods(List<String> foods) {
+        Food.foods = foods;
+    }
+
+    private void getAllWoods(){
+        foods.add("Soup");
+        foods.add("Fish");
+        foods.add("Ravioli");
+        foods.add("Meat");
+        foods.add("Burger");
+        foods.add("Cake");
+        foods.add("French fries");
+        foods.add("Bratwurst");
+        foods.add("Lasagna");
+        foods.add("Carpaccio");
+        foods.add("Brioche");
+        foods.add("Ratatouille");
     }
 }
