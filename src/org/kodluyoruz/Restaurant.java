5944311c8df159639c2ab48558825c05c30b37ba
Syr7-s <isasayar7@github.com>
2021-01-14 14:59:37 +0300

    OrderList and orderPlaced variables was created.

--- src/org/kodluyoruz/Restaurant.java
+++ src/org/kodluyoruz/Restaurant.java
@@ -1,9 +1,16 @@
 package org.kodluyoruz;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Restaurant {
     static final int RESTAURANT_TABLE_COUNT = 5;
     static final int RESTAURANT_WAITER_COUNT = 3;
     static final int RESTAURANT_COOK_COUNT = 2;
+
+    static LinkedList<Integer> orderList = new LinkedList<>();
+    private static HashMap<Integer, String> orderPlaced = new HashMap<>();
+
 
     static int customerCount;
 
