4f7a7100b58f609c7319d046c0d305237add0575
Syr7-s <isasayar7@github.com>
2021-01-14 14:49:27 +0300

    Necessary variables, 2 parameters constructor and toString method was created for Cook Class.

--- src/org/kodluyoruz/Cook.java
+++ src/org/kodluyoruz/Cook.java
@@ -1,4 +1,20 @@
 package org.kodluyoruz;
 
+import java.util.concurrent.Semaphore;
+
 public class Cook {
+    private String cookName;
+    private int orderNum;
+    private Semaphore semaphore;
+    private String food;
+
+    public Cook(String cookName, Semaphore semaphore) {
+        this.cookName = cookName;
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public String toString() {
+        return this.cookName;
+    }
 }
