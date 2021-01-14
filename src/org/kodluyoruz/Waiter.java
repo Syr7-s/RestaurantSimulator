3044dc19239fb0bc3f0c2656c45266ab5c750da7
Syr7-s <isasayar7@github.com>
2021-01-14 16:35:04 +0300

    Run method was edited in the Waiter Class

--- src/org/kodluyoruz/Waiter.java
+++ src/org/kodluyoruz/Waiter.java
@@ -19,7 +19,36 @@ public class Waiter implements Runnable {
 
     @Override
     public void run() {
+        try {
+            semaphore.acquire();
+            System.out.println(this.waiterName + " waiter will take the order");
+            for (int i = 0; i <= Restaurant.orderList.size(); i++) {
+                if (Restaurant.orderAvailable(this)) {
+                    orderNum = Restaurant.waiterGetOrderNum(this);
+                    food = Restaurant.waiterGetOrder(this);
+                    System.out.println("Waiter named "+this +"received the order of "+food+" number "+ orderNum);
+                } else {
+                    System.out.println("Order is not");
+                }
 
+                Thread.sleep(2000);
+                boolean waiterWaiting = true;
+                while (waiterWaiting) {
+                    waiterWaiting = false;
+                    Thread.sleep(300);
+                    if (Restaurant.checkWaiterOrderStatus(orderNum)) {
+                        Restaurant.waiterOrderCompleted(this, orderNum);
+                    } else {
+                        System.out.println("Waiter named "+this +" did not receive the order of "+food+" number "+ orderNum+" from Cook");
+                    }
+                    System.out.println("Waiter named "+this+" job is over.");
+                }
+            }
+        } catch (InterruptedException exception) {
+            System.out.println("Waiter did not receive the order from Cook");
+
+        }
+        semaphore.release();
 
 
     }
