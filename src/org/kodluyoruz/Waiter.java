59a6b1436ad1dcacfa895c63467de5b1e6c69d38
Syr7-s <isasayar7@github.com>
2021-01-14 14:44:24 +0300

    Necessary variables was created for Waiter Class.

--- src/org/kodluyoruz/Waiter.java
+++ src/org/kodluyoruz/Waiter.java
@@ -1,4 +1,11 @@
 package org.kodluyoruz;
 
+import java.util.concurrent.Semaphore;
+
 public class Waiter {
+    private String waiterName;
+    private int orderNum;
+    private Semaphore semaphore;
+
+
 }
