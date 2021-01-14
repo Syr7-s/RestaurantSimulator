78116260704e208caf5a6e3ded3d1626e494cff2
Syr7-s <isasayar7@github.com>
2021-01-14 14:26:02 +0300

    Restaurant variables were created.

--- src/org/kodluyoruz/Restaurant.java
+++ src/org/kodluyoruz/Restaurant.java
@@ -1,5 +1,9 @@
 package org.kodluyoruz;
 
 public class Restaurant {
+    static final int RESTAURANT_TABLE_COUNT = 5;
+    static final int RESTAURANT_WAITER_COUNT = 3;
+    static final int RESTAURANT_COOK_COUNT =2;
+
 
 }
