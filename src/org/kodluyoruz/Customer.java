fe692984a439c4514c68479c96de7be1e20f562c
Syr7-s <isasayar7@github.com>
2021-01-14 14:42:37 +0300

    Necessary variables was created for Customer class.And Three parameter constructor was created.

--- src/org/kodluyoruz/Customer.java
+++ src/org/kodluyoruz/Customer.java
@@ -1,4 +1,26 @@
 package org.kodluyoruz;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class Customer {
+    Random random = new Random();
+
+    private Food foodInstance = Food.getInstance();
+
+    private String customerName;
+    private int customerOrderNumber;
+    private String food;
+    private Semaphore semaphore;
+
+    public Customer(String customerName, int customerOrderNumber, Semaphore semaphore) {
+        this.customerName = customerName;
+        this.customerOrderNumber = customerOrderNumber;
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public String toString() {
+        return this.customerName;
+    }
 }
