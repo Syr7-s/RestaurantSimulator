ecd7eca8724e9288182f0fd1afa6ff50c8841150
Syr7-s <isasayar7@github.com>
2021-01-15 19:22:08 +0300

    Main class was edited.

--- src/org/kodluyoruz/main/Main.java
+++ src/org/kodluyoruz/main/Main.java
@@ -10,15 +10,15 @@ public class Main {
         // write your code here
         RestaurantAppContext restaurantAppContext = new RestaurantAppContext();
         Food food = restaurantAppContext.food();
-        System.out.println("There are "+ Restaurant.RESTAURANT_TABLE_COUNT+" tables in the restaurant.\n" +
-                "Restaurant customer capacity is "+Restaurant.CUSTOMER_COUNT +".\n"+Restaurant.RESTAURANT_WAITER_COUNT+
-                " waiters and "+ Restaurant.RESTAURANT_COOK_COUNT +" cooks work in the Restaurant.\n" +
+        System.out.println("There are " + Restaurant.RESTAURANT_TABLE_COUNT + " tables in the restaurant.\n" +
+                "Restaurant customer capacity is " + Restaurant.CUSTOMER_COUNT + ".\n" + Restaurant.RESTAURANT_WAITER_COUNT +
+                " waiters and " + Restaurant.RESTAURANT_COOK_COUNT + " cooks work in the Restaurant.\n" +
                 "Food Types: \n" +
-                "----------------------------------------------\n"+
-                food.getFoods()+"\n" +
+                "----------------------------------------------\n" +
+                food.getFoods() + "\n" +
                 "----------------------------------------------");
 
-        restaurantAppContext.restaurant(restaurantAppContext.restaurantSimulation(food));
+        Restaurant restaurant = restaurantAppContext.restaurant(restaurantAppContext.restaurantSimulation(food));
 
     }
 }
