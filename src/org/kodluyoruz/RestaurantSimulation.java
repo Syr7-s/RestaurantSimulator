05404c0c0b057e54f59715b1e62a67765cfd8209
Syr7-s <isasayar7@github.com>
2021-01-15 14:51:28 +0300

    Semaphore and ExecutorService variables was added.

--- src/org/kodluyoruz/RestaurantSimulation.java
+++ src/org/kodluyoruz/RestaurantSimulation.java
@@ -1,4 +1,17 @@
 package org.kodluyoruz;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 public class RestaurantSimulation {
+    private Semaphore semaphoreCustomer = new Semaphore(Restaurant.RESTAURANT_TABLE_COUNT, false);
+    private Semaphore semaphoreWaiter = new Semaphore(Restaurant.RESTAURANT_WAITER_COUNT, true);
+    private Semaphore semaphoreCooker = new Semaphore(Restaurant.RESTAURANT_COOK_COUNT, true);
+
+    private static final ExecutorService executorServiceCustomer = Executors.newFixedThreadPool(Restaurant.RESTAURANT_TABLE_COUNT);
+    private static final ExecutorService executorServiceWaiter = Executors.newFixedThreadPool(Restaurant.RESTAURANT_WAITER_COUNT);
+    private static final ExecutorService executorServiceCook = Executors.newFixedThreadPool(Restaurant.RESTAURANT_COOK_COUNT);
+
+
 }
